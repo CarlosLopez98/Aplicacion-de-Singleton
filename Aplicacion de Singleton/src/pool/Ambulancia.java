e3015b5b498067909eff7360f9d75ee2deddafd0
Carlos <Carlos@DESKTOP-CKVHQM9>
2018-02-20 01:27:11 -0500

    Clase ambulancia implementada

---
+++ Aplicacion de Singleton/src/pool/Ambulancia.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pool;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Ambulancia {
+    
+    public int numero;
+    public boolean estado;
+    
+    public Ambulancia(int n){
+        numero = n;
+        estado = true;
+    }
+    
+    public boolean getEstado(){
+        return estado;
+    }
+    
+    public void setEstado(boolean e){
+        estado = e;
+    }
+    
+    public int getNumero(){
+        return numero;
+    }
+    
+    public void setNumero(int n){
+        numero = n;
+    }
+}
